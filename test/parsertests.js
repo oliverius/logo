9d29f73726891deb3cf50af7ca07e4996af2e8ab
oliverius <oliverius@outlook.com>
2020-05-27 16:40:32 +0100

    Trying events in parsertests

--- test/parsertests.js
+++ test/parsertests.js
@@ -1,21 +1,40 @@
 runParserTests();
 function runParserTests() {
-    let tokenizer = new Tokenizer();
-    let parser = new Parser();
+    let parserTestEventName = "PARSER_TEST_EVENT";
+    const tokenizer = new Tokenizer();
+    const parser = new Parser();
+    let events = [];
+    let raiseParserTestEvent = () => {
+        let event = new CustomEvent(parserTestEventName);
+        window.dispatchEvent(event);
+        console.log("dispatch", event);
+    }
     let parserEventsStream = (event) => {
-        console.log(event.detail);
+        switch(event.type) {
+            case parserTestEventName:
+                events = [];
+                break;
+            case parser.parserStatusEventName():
+                console.log("get status");
+                break;
+            case parser.turtleDrawingQueueEventName():
+                console.log("turtle");
+                break;
+        }
+        console.log(event.type,  event.detail);
     }
     window.addEventListener(parser.parserStatusEventName(), parserEventsStream, false);
     window.addEventListener(parser.turtleDrawingQueueEventName(), parserEventsStream, false);
-    let assertParser = (script = "") => {
+    window.addEventListener(parserTestEventName, parserEventsStream, false);
+    let assertScript = (script = "") => {
+        raiseParserTestEvent();
         let tokens = tokenizer.tokenize(script);
         parser.parse(tokens);
     }
-    
 
 
 
 
-    //assertParser("fd 60");
-    assertParser("repeat 4 [fd 60 rt 90]");
+    assertScript("fd 60");
+    //assertScript("repeat 4 [fd 60 rt 90]");
 }
